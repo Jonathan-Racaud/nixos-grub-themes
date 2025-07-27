2bbea8c674fe73221cd7b4512192d796fda43139
Jonathan Racaud <contact@jracaud.com>
2025-07-27 13:37:02 +0900

    Trying to fix hash issue by removing it

--- themes/space-isolation/default.nix
+++ themes/space-isolation/default.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
     owner = "callmenoodles";
     repo = "space-isolation";
     rev = "v0.2.0";
-    hash = "f6a11eb7edb22ba3959b944d68224f2f6f892457";
+    #hash = "f6a11eb7edb22ba3959b944d68224f2f6f892457";
   };
   installPhase = "cp -r 1920x1080 $out";
 }
